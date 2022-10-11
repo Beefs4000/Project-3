6d88c8f786c0bfb47489d33388aa009b09e96783
Beefs4000 <103645633+Beefs4000@users.noreply.github.com>
2022-10-11 21:32:35 +0800

    2nd day

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// buildSchema will take a string and converts it to the required java script
-const { buildSchema } = require('graphql');
 
 // graphqlHttp exports a valid middleware function, taking incomming requests, funnels them through graphql 
 // query parser and auto-forwards to the correct resolvers
-const graphqlHttp = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 
+// buildSchema will take a string and converts it to the required java script
+const { buildSchema } = require('graphql');
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const Event = require('./models/event')
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
+
 
 // middleware to parse Json bodies
 app.use(bodyParser.json());
 
 // 
-app.use(
-    '/graphql', 
-    graphqlHttp({
+app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-                events: [String!]!
+                events: [Event!]!
         }
 
         type RootMutation{
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -35,16 +52,53 @@ app.use(
     `),
     rootvalue: {
         events: () => {
-            return ['Romantic cooking', 'Sailng', 'All-Night Coding'];
+           return Event.find()
+            .then(events => {
+                // use map to transform events returned into new object minus meta data
+                return events.map(event => {
+                    return { ...event._doc, _id: event.doc._id.toString()};
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
         },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+        createEvent: args => {
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date,
+
+            // };
+            // console.log(args);
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+            });
+            return event.save().then(result => {
+                console.log(result);
+                // use spread operator to return object minus meta data
+                return {...result._doc, _id: result.doc._id.toString()};
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
         }
     },
     graphiql: true
 }));
 
-app.listen(PORT, () => {
-    console.log(`API server running on http://localhost:${PORT}`);
-  });
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:
+${process.env.MONGO_PASSWORD}
+@cluster0.olaqxjp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`);
+      });
+}).catch(err => {
+    console.log(err);
+})
